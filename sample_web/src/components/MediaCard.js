b12b63c3ed0b2b1c2598e0b31c5e4701a6704eb8
saltp40 <saltp40@gmail.com>
2020-02-18 23:27:40 +0900

    :art: Change chip position in MediaCard

--- sample_web/src/components/MediaCard.js
+++ sample_web/src/components/MediaCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,6 +8,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+  cardAction: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+});
+
 const MediaCard = ({
   data = {
     title: "Title",
@@ -14,33 +22,36 @@ const MediaCard = ({
     category: {name: "", color: ""}
   }
 }) => {
+  const classes = useStyles();
   return (
     <Card>
       <CardActionArea>
         <CardContent>
-          <Chip
-            variant="outlined"
-            size="small"
-            label={data.category.name}
-            style={{
-              color: data.category.color,
-              borderColor: data.category.color
-            }}
-          />
-          <Typography gutterBottom variant="h6">
+          <Typography variant="h6">
             {data.title}
           </Typography>
           <Typography
             variant="body2"
             color="textSecondary"
-            component="p"
           >
             {data.text}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
+      <CardActions className={classes.cardAction}>
+        <Chip
+          variant="outlined"
+          size="small"
+          label={data.category.name}
+          style={{
+            color: data.category.color,
+            borderColor: data.category.color
+          }}
+        />
+        <Button
+          size="small"
+          color="primary"
+        >
           Google検索
         </Button>
       </CardActions>
