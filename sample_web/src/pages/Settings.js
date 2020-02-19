b17d88b993ec855e28deb4ca51d65650eb89678d
saltp40 <saltp40@gmail.com>
2020-02-19 23:49:04 +0900

    :rotating_light: Comment out the code in Setting.js for warning messages

--- sample_web/src/pages/Settings.js
+++ sample_web/src/pages/Settings.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import SettingForm from '../components/UserSettingForm';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import SettingForm from '../components/UserSettingForm';
+// import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+// import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+// import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const styles = theme => ({
 });
