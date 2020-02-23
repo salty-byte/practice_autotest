88b88194a509c5a36853e89cc157d15e99e25d3e
saltp40 <saltp40@gmail.com>
2020-02-24 01:17:31 +0900

    Updates

---
+++ precache-manifest.6ba19d0f84cd5fe28fe8bfe02c7106b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853bf03c5c077022f2e8d7f5d5e1d552",
+    "url": "/practice_autotest/index.html"
+  },
+  {
+    "revision": "6e4476237530a016a08c",
+    "url": "/practice_autotest/static/css/main.821e99de.chunk.css"
+  },
+  {
+    "revision": "836d6ea8e1da2cf8a444",
+    "url": "/practice_autotest/static/js/2.6b6cac9f.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/practice_autotest/static/js/2.6b6cac9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4476237530a016a08c",
+    "url": "/practice_autotest/static/js/main.40c93759.chunk.js"
+  },
+  {
+    "revision": "0d5d49bc915bfa437961",
+    "url": "/practice_autotest/static/js/runtime-main.de3708f2.js"
+  },
+  {
+    "revision": "9e79eff198c77cfa97dc1fd7ef68ce04",
+    "url": "/practice_autotest/static/media/logo.9e79eff1.png"
+  }
+]);
